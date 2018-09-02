a050fd1caf68a350a62c6c08fd6ff98524304554
Geoff Whitehead <geoff1012@gmail.com>
2018-09-02 10:36:39 +0100

    adds basic client side files

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => {
+  return <div>Connect4</div>;
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
