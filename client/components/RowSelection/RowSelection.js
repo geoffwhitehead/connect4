d3227038037194f6b1f19054a073499bd2642fb1
Geoff Whitehead <geoff1012@gmail.com>
2018-09-06 03:12:42 +0100

    adds disabled state to row select

--- client/components/RowSelection/RowSelection.js
+++ client/components/RowSelection/RowSelection.js
@@ -1,17 +1,6 @@
 import React from "react";
 import css from "./RowSelection.scss";
-import Animation from "react-lottie";
-import * as animationData from "assets/animations/mappoint.json";
 import ButtonAnimatedIcon from "components/common/ButtonAnimatedIcon/ButtonAnimatedIcon";
-
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData: animationData,
-  rendererSettings: {
-    preserveAspectRatio: "xMidYMid slice"
-  }
-};
 
 export default ({ selectColumn, disabled }) => {
   const handleSelect = index => !disabled && selectColumn(index);
@@ -20,22 +9,12 @@ export default ({ selectColumn, disabled }) => {
     let columns = [];
     for (let i = 0; i < 7; i++) {
       columns.push(
-        // <div className={css.column} onClick={handleSelect.bind(this, i)}>
-        //   {/* <Animation
-        //     options={defaultOptions}
-        //     onClick={null}
-        //     isClickToPauseDisabled
-        //     height={75}
-        //     width={75}
-        //   /> */}
-        // </div>
-        // <div className={css.column} onClick={handleSelect.bind(this, i)}>
         <ButtonAnimatedIcon
+          disabled={disabled}
           iconName="angle double down"
           text="Drop"
           onClick={handleSelect.bind(this, i)}
         />
-        // </div>
       );
     }
     return columns;
